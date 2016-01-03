79ed76bf59e0e30c325e43f3c35128406be47951
Moshe Edri <epotexg@gmail.com>
2016-01-03 12:47:30 -0800

    Smartthings - ISY + Global Cache IR control

---
+++ smartapps/gcwf2ir/soundbar-on-off.src/soundbar-on-off.groovy
@@ -0,0 +1,47 @@
+
+
+definition(
+name: "Soundbar On/Off",
+namespace: "GCWF2IR",
+author: "Epotex",
+description: "Controll IR",
+category: "My Apps",
+iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+    section("Choose Virtuall switch that will controll the IR") {
+    	input "trigger", "capability.switch", title: "Which switch?", required: true
+        log.debug "Selected"
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+	subscribe(trigger, "switch.on", switchOnHandler)
+	subscribe(trigger, "switch.off", switchOffHandler)
+}
+
+def switchOnHandler(evt) {
+	log.debug "ON"
+    def deviceNetworkId = "C0A800FA:1386"   
+    def theCom = "sendir,1:1,1,38109,1,1,171,170,19,19,19,19,19,19,19,19,19,57,19,57,19,19,19,19,19,57,19,57,19,57,19,57,19,19,19,19,19,19,19,19,19,171,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,57,19,57,19,57,19,19,19,57,19,57,19,57,19,57,19,19,19,19,19,19,19,57,19,3810"
+    sendHubCommand(new physicalgraph.device.HubAction("""$theCom\r\n""",  physicalgraph.device.Protocol.LAN, deviceNetworkId))
+}
+
+def switchOffHandler(evt) {
+	log.debug "OFF"
+    def deviceNetworkId = "C0A800FA:1386"   
+    def theCom = "sendir,1:1,1,38109,1,1,171,170,19,19,19,19,19,19,19,19,19,57,19,57,19,19,19,19,19,57,19,57,19,57,19,57,19,19,19,19,19,19,19,19,19,171,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,19,57,19,57,19,57,19,19,19,57,19,57,19,57,19,57,19,19,19,19,19,19,19,57,19,3810"
+    sendHubCommand(new physicalgraph.device.HubAction("""$theCom\r\n""",  physicalgraph.device.Protocol.LAN, deviceNetworkId))
+}
